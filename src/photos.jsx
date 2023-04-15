e269115b17547709ef6b823e4e6cacffb8f1b3bc
Ben Schwartz <btschwartz12@gmail.com>
2023-04-14 21:29:31 -0400

    More photos

--- src/photos.jsx
+++ src/photos.jsx
@@ -29,6 +29,126 @@ const photos = [
         width: 400,
         height: 300
     },
+    { 
+        src: "/pics/static/images/1.jpg",
+        width: 300,
+        height: 400
+    },
+    {
+        src: "/pics/static/images/2.jpg",
+        width: 300,
+        height: 400
+    },
+    { 
+        src: "/pics/static/images/3.jpg",
+        width: 300,
+        height: 400
+    },
+    {
+        src: "/pics/static/images/4.jpg",
+        width: 300,
+        height: 400
+    },
+    {
+        src: "/pics/static/images/5.jpg",
+        width: 750,
+        height: 1000
+    },
+    {
+        src: "/pics/static/images/6.png",
+        width: 300,
+        height: 400
+    },
+    {
+        src: "/pics/static/images/7.png",
+        width: 750,
+        height: 1000
+    },
+    {
+        src: "/pics/static/images/8.jpg",
+        width: 300,
+        height: 400
+    },
+    { 
+        src: "/pics/static/images/9.jpg",
+        width: 300,
+        height: 400
+    },
+    {
+        src: "/pics/static/images/10.jpg",
+        width: 300,
+        height: 400
+    },
+    {
+        src: "/pics/static/images/11.jpg",
+        width: 750,
+        height: 1000
+    },
+    {
+        src: "/pics/static/images/12.jpg",
+        width: 300,
+        height: 400
+    },
+    {
+        src: "/pics/static/images/13.jpg",
+        width: 300,
+        height: 400
+    },
+    {
+        src: "/pics/static/images/14.jpg",
+        width: 300,
+        height: 400
+    },
+    {
+        src: "/pics/static/images/15.jpg",
+        width: 300,
+        height: 400
+    },
+    {
+        src: "/pics/static/images/16.jpg",
+        width: 300,
+        height: 400
+    },
+    { 
+        src: "/pics/static/images/17.jpg",
+        width: 300,
+        height: 400
+    },
+    {
+        src: "/pics/static/images/18.jpg",
+        width: 300,
+        height: 400
+    },
+    {
+        src: "/pics/static/images/19.jpg",
+        width: 300,
+        height: 400
+    },
+    {
+        src: "/pics/static/images/20.jpg",
+        width: 300,
+        height: 400
+    },
+    {
+        src: "/pics/static/images/21.jpg",
+        width: 750,
+        height: 1000
+    },
+    {
+        src: "/pics/static/images/22.png",
+        width: 300,
+        height: 400
+    },
+    {
+        src: "/pics/static/images/23.jpg",
+        width: 750,
+        height: 1000
+    },
+    {
+        src: "/pics/static/images/24.jpg",
+        width: 300,
+        height: 400
+    }
 ];
 
 
